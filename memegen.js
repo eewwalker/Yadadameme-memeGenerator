03480fdf4fd58a9b8a43aef3666fea482215229c
Emily Walker <ewalker415@gmail.com>
2024-01-19 20:34:40 -0800

    more styling and JS functionality

--- memegen.js
+++ memegen.js
@@ -4,11 +4,34 @@ const bottomText = document.querySelector('#bottom-text');
 const submitBtn = document.querySelector('#submit-btn');
 const memeSection = document.querySelector('#section-memes');
 
-submitBtn.addEventListener('click', getMeme);
+submitBtn.addEventListener('click', createMeme);
 
-function getMeme(e) {
+function createMeme(e) {
   e.preventDefault();
-  if (!imgUrl.value || !topText.value || !bottomText.value) {
-    alert('Please enter all fields!');
+  if (!imgUrl.value || (!topText.value && !bottomText.value)) {
+    alert('Please enter URL and at least one text field!');
+    return;
   }
+  const imgContainer = document.createElement('div');
+  const memeImg = document.createElement('img');
+  const topTextDiv = document.createElement('div');
+  const bottomTextDiv = document.createElement('div');
+
+  topTextDiv.appendChild(document.createTextNode(topText.value));
+  bottomTextDiv.appendChild(document.createTextNode(bottomText.value));
+
+  memeImg.setAttribute('src', imgUrl.value);
+  memeImg.classList.add('meme-img');
+
+  imgContainer.classList.add('img-container');
+  topTextDiv.classList.add('top-text');
+  bottomTextDiv.classList.add('bottom-text');
+
+  imgContainer.appendChild(topTextDiv);
+  imgContainer.appendChild(bottomTextDiv);
+  imgContainer.appendChild(memeImg);
+  memeSection.appendChild(imgContainer);
+  imgUrl.value = '';
+  topText.value = '';
+  bottomText.value = '';
 }
